7993989b0614a9dc5bc8ceba2200fc400bd64ea2
zyz954489346 <954489346zyz@sina.com>
2017-11-08 16:15:26 +0800

    2017年11月3日15:34:34

---
+++ public/static/admin/js/category.js
@@ -0,0 +1,38 @@
+/**
+ * Created by xs-01 on 2017/11/8.
+ */
+$(function(){
+    //初始化页面表格
+    var oTable = new TableInit();
+    //表格列参数
+    var columns = [
+        {
+            field : 'checked',
+            checkbox : false
+        },
+        {
+            field:'id',
+            title:'编号'
+        },
+        {
+            field:'name',
+            title:'分类名',
+            sortable:true
+
+        },
+        {
+            field:'create_time',
+            title:'创建时间',
+            sortable:true
+        },
+        {
+            field:'id',
+            title:'操作',
+            formatter:function(value,row,index){
+                return "<button class='btn btn-sm btn-danger' data-id='"+row.id+"'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span>删除</button>";
+            }
+        }
+    ];
+    oTable.Init("#content_table",SCOPE.get_table_url,columns,"#toolbar");
+});
+
---
+++ public/static/admin/js/common.js
@@ -0,0 +1,54 @@
+/**
+ * Created by xs-01 on 2017/11/8.
+ */
+//公共js
+/**
+ * 初始化bootstrap-table表格
+ * @type {{Init: oTableInit.Init, queryParams: oTableInit.queryParams}}
+ * @param ele 表格元素
+ * @param url 表格数据请求地址
+ * @param columns 列参数json
+ * @param toolbar 工具栏元素
+ */
+var TableInit = function(){
+    var oTableInit = {
+        Init : function(ele,url,columns,toolbar){
+            $(ele).bootstrapTable({
+                url: url,
+                method: 'post',
+                striped:true,
+                toolbar: toolbar ? toolbar : undefined,
+                cache:false,
+                pagination:true,
+                sortable:true,                      //是否启用排序
+                undefinedText:'无',
+                sortOrder:'desc',
+                queryParams:oTableInit.queryParams, //传递参数
+                sidePagination:'server',
+                pageNumber:1,
+                pageSize:15,
+                pageList: [5,10,15,25,50,100],
+                search: true,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+                strictSearch: true,
+                showColumns: true,                  //是否显示所有的列
+                showRefresh: true,                  //是否显示刷新按钮
+                clickToSelect: true,
+                uniqueId: "id",                     //每一行的唯一标识，一般为主键列
+                showToggle:true,                    //是否显示详细视图和列表视图的切换按钮
+                cardView: false,                    //是否显示详细视图
+                columns: columns,                   //列数据
+                locales: "zh-CN"
+            });
+        },
+        queryParams : function(params){
+            return {
+                limit : params.limit,   //页面大小
+                offset: params.offset,  //页码
+                order : params.order,   //排序规则
+                sort  : params.sort,    //排序字段名
+                search: params.search   //搜索参数
+            };
+        }
+    };
+    return oTableInit;
+};
