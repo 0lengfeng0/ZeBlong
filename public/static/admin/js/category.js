4db6b09787f69ce32fb77a8cdb9e511afa6c6c28
zyz954489346 <954489346zyz@sina.com>
2017-11-09 10:47:30 +0800

    2017年11月3日15:34:34

--- public/static/admin/js/category.js
+++ public/static/admin/js/category.js
@@ -29,10 +29,29 @@ $(function(){
             field:'id',
             title:'操作',
             formatter:function(value,row,index){
-                return "<button class='btn btn-sm btn-danger' data-id='"+row.id+"'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span>删除</button>";
+                return "<button class='btn btn-sm btn-danger del_cat' data-id='"+row.id+"'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span>删除</button>";
             }
         }
     ];
     oTable.Init("#content_table",SCOPE.get_table_url,columns,"#toolbar");
+
+    //表格单行删除按钮
+    $(document).on('click','del_cat',function(){
+        var id = $(this).data('id');     //数据id
+        $.ajax({
+            type:'POST',
+            data:{'id':$id},
+            url :SCOPE.del_td_url,
+            dateType:'json',
+            success: function(res){
+                if(res.status == false){
+
+                }else{
+
+                }
+            }
+
+        })
+    });
 });
 
