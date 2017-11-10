c7437ac0620043969243509367ba122d6348a517
zyz954489346 <954489346zyz@sina.com>
2017-11-10 10:45:44 +0800

    2017年11月3日15:34:34

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -15,28 +15,70 @@ class Login extends Controller
             return redirect('Index/index');
         }
         //验证是否存在cookie
-
+        if(!empty(cookie("ZeBlong_admin_info"))){
+            $cookie_info = cookie("ZeBlong_admin_info");
+            $user = new User();
+            $res =  $user->getUserInfo(['name'=>$cookie_info['name']],'name,pwd,salt,is_admin,status');
+            if(!empty($res) && $res->status != 2){
+                if($res->pwd == $cookie_info['pwd']){
+                    session('ZeBlong_admin_info',$cookie_info);
+                    return $this->redirect('Index/index');
+                }
+            }
+            cookie("ZeBlong_admin_info",null);
+        }
 
         if($this->request->isPost()){
-            $user = new User();
             //登录验证
             $name = input('post.name');
             if(empty($name)){
                 return show(false,'请输入用户名');
             }
-            //先查询用户信息
-            $user_info = $user->getUserInfo(['name'=>$name],'pwd,salt,id_admin,status');
-            if(empty($user_info)){
-                return show(false,'登录失败');
-            }
-            
-
             $pwd = input('post.pwd');
             if(empty($pwd)){
                 return show(false,'请输入密码');
             }
             $remember = input('post.remember');
+            //验证密码
+            $user_info = $this->checkLog($name,$pwd);
+            if(!empty($user_info->pwd)){
+                //登陆成功，存session
+                $save_arr = [
+                    'name'  =>  $user_info->name,
+                    'pwd'   =>  $user_info->pwd,
+                ];
+                session('ZeBlong_admin_info',$save_arr);
+                if($remember){
+                    //记住密码[cookie],7天过期
+                    cookie("ZeBlong_admin_info",$save_arr,604800);
+                }
+                return show(true,'登录成功');
+            }else{
+                return $user_info;
+            }
         }
         return $this->fetch();
+    }
+
+    /**
+     * 登录验证
+     */
+    private function checkLog($name,$pwd)
+    {
+        $user = new User();
+        //先查询用户信息
+        $user_info = $user->getUserInfo(['name'=>$name],'name,pwd,salt,is_admin,status');
+        if(empty($user_info)){
+            return show(false,'用户不存在');
+        }
+        if($user_info->status == 2){
+            return show(false,"账户已被封禁");
+        }
+        $md5pwd = md5Str(trim($pwd),$user_info->salt);
+        if($md5pwd != $user_info->pwd){
+            return show(false,'密码错误');
+        }
+
+        return $user_info;
     }
 }
--- public/static/admin/js/login.js
+++ public/static/admin/js/login.js
@@ -1,10 +1,34 @@
 $(function(){
     $("#login_btn").click(function(){
+        //表单验证
+        if(!$('input[name="name"]').val()){
+            $('input[name="name"]').parent(".input-group").addClass('has-error');
+        }else{
+            $('input[name="name"]').parent(".input-group").removeClass('has-error');
+        }
+        if(!$('input[name="pwd"]').val()){
+            $('input[name="pwd"]').parent(".input-group").addClass('has-error');
+        }else{
+            $('input[name="pwd"]').parent(".input-group").removeClass('has-error');
+        }
         var data = {
             'name'      :   $('input[name="name"]').val(),
             'pwd'       :   $('input[name="pwd"]').val(),
-            'remember'  :   $('input[name="remember"]').attr('checked')
-        }
-
+            'remember'  :   $('input[name="remember"]').is(':checked')
+        };
+        //发post
+        $.ajax({
+            type:'POST',
+            data:data,
+            url:SCOPE.login_url,
+            dataType:'json',
+            success:function(res){
+                if(res.status == false){
+                    layer.msg(res.msg,{icon:5});
+                }else{
+                    window.location.href = SCOPE.index_url;
+                }
+            }
+        })
     });
 });
