6da478b9597b037f6d933abea15550fe43e2445a
zyz954489346 <954489346zyz@sina.com>
2017-11-10 11:01:39 +0800

    2017年11月3日15:34:34

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -18,11 +18,13 @@ class Login extends Controller
         if(!empty(cookie("ZeBlong_admin_info"))){
             $cookie_info = cookie("ZeBlong_admin_info");
             $user = new User();
-            $res =  $user->getUserInfo(['name'=>$cookie_info['name']],'name,pwd,salt,is_admin,status');
+            $res =  $user->getUserInfo(['name'=>$cookie_info['name']],'id,name,pwd,salt,is_admin,status,log_time');
             if(!empty($res) && $res->status != 2){
                 if($res->pwd == $cookie_info['pwd']){
-                    session('ZeBlong_admin_info',$cookie_info);
-                    return $this->redirect('Index/index');
+                    if($this->changeLogTime($res->id)){
+                        session('ZeBlong_admin_info',$cookie_info);
+                        return $this->redirect('Index/index');
+                    }
                 }
             }
             cookie("ZeBlong_admin_info",null);
@@ -42,15 +44,22 @@ class Login extends Controller
             //验证密码
             $user_info = $this->checkLog($name,$pwd);
             if(!empty($user_info->pwd)){
-                //登陆成功，存session
+                //登陆成功,修改log_time
+                if(!$this->changeLogTime($user_info->id)){
+                    return show(false,'登录失败');
+                }
+                //存session
                 $save_arr = [
                     'name'  =>  $user_info->name,
                     'pwd'   =>  $user_info->pwd,
+                    'log_time'  =>  $user_info->log_time
                 ];
                 session('ZeBlong_admin_info',$save_arr);
                 if($remember){
                     //记住密码[cookie],7天过期
                     cookie("ZeBlong_admin_info",$save_arr,604800);
+                }else{
+                    cookie("ZeBlong_admin_info",null);
                 }
                 return show(true,'登录成功');
             }else{
@@ -61,13 +70,26 @@ class Login extends Controller
     }
 
     /**
+     * 退出登录
+     */
+    public function logOut()
+    {
+        session("ZeBlong_admin_info",null);
+        cookie("ZeBlong_admin_info",null);
+        return $this->redirect('Login/index');
+    }
+
+
+    //--------------------------------------------------------------------
+
+    /**
      * 登录验证
      */
     private function checkLog($name,$pwd)
     {
         $user = new User();
         //先查询用户信息
-        $user_info = $user->getUserInfo(['name'=>$name],'name,pwd,salt,is_admin,status');
+        $user_info = $user->getUserInfo(['name'=>$name],'id,name,pwd,salt,is_admin,status,log_time');
         if(empty($user_info)){
             return show(false,'用户不存在');
         }
@@ -80,5 +102,20 @@ class Login extends Controller
         }
 
         return $user_info;
+    }
+
+    /**
+     * 登陆后修改登录时间
+     */
+    private function changeLogTime($id)
+    {
+        $user = new User();
+        $condition = ['id'=>$id];
+        $data = ['log_time'=>time()];
+        $edit_res = $user->editUserInfo($condition,$data);
+        if(empty($edit_res)){
+            return false;
+        }
+        return true;
     }
 }
--- application/common/model/User.php
+++ application/common/model/User.php
@@ -13,4 +13,16 @@ class User extends Model
         $res = $this->where($condition)->field($field)->find();
         return $res;
     }
+
+    /**
+     * 修改一条数据
+     */
+    public function editUserInfo($condition=[],$data=[])
+    {
+        if(!is_array($condition) || !is_array($data) || empty($data)){
+            exception("获取修改信息失败");
+        }
+        $res = $this->where($condition)->update($data);
+        return $res;
+    }
 }
